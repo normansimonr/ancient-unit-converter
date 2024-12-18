78998f763dec2bc6676374bd9206a9de673e71b6
normansimonr <normansimonr@gmail.com>
2024-12-18 14:02:26 -0500

    Add all length units

--- script.js
+++ script.js
@@ -17,12 +17,44 @@ const conversionTable = {
       yards: CUBIT_IN_YARDS
     }
   },
-  talent: {
-    category: "weight",
+  long_cubit: {
+    category: "length",
     conversions: {
-      kilograms: 34.3, // For simplicity, this could also be derived from a base weight unit if applicable
-      grams: 34.3 * 1000,
-      pounds: 34.3 * 2.20462
+      kilometers: 7 * CUBIT_IN_KILOMETERS / 6, // A long cubit is seven handbreadths
+      meters: 7 * CUBIT_IN_METERS / 6,
+      centimeters: 7 * CUBIT_IN_CM / 6,
+      inches: 7 * CUBIT_IN_INCHES / 6,
+      yards: 7 * CUBIT_IN_YARDS / 6
+    }
+  },
+  finger: {
+    category: "length",
+    conversions: {
+      kilometers: CUBIT_IN_KILOMETERS / 24, // 1 finger = 1/24 cubit
+      meters: CUBIT_IN_METERS / 24,
+      centimeters: CUBIT_IN_CM / 24,
+      inches: CUBIT_IN_INCHES / 24,
+      yards: CUBIT_IN_YARDS / 24
+    }
+  },
+  cubit_gmd: {
+    category: "length",
+    conversions: {
+      kilometers: CUBIT_IN_KILOMETERS,
+      meters: CUBIT_IN_METERS,
+      centimeters: CUBIT_IN_CM,
+      inches: CUBIT_IN_INCHES,
+      yards: CUBIT_IN_YARDS
+    }
+  },
+  span: {
+    category: "length",
+    conversions: {
+      kilometers: CUBIT_IN_KILOMETERS / 2, // 1 span = 1/2 cubit
+      meters: CUBIT_IN_METERS / 2, 
+      centimeters: CUBIT_IN_CM / 2,
+      inches: CUBIT_IN_INCHES / 2,
+      yards: CUBIT_IN_YARDS / 2
     }
   },
   handbreadth: {
@@ -35,16 +67,74 @@ const conversionTable = {
       yards: CUBIT_IN_YARDS / 6
     }
   },
-  finger: {
+  reed: {
     category: "length",
     conversions: {
-      kilometers: CUBIT_IN_KILOMETERS / 24, // 1 finger = 1/24 cubit
-      meters: CUBIT_IN_METERS / 24,
-      centimeters: CUBIT_IN_CM / 24,
-      inches: CUBIT_IN_INCHES / 24,
-      yards: CUBIT_IN_YARDS / 24
+      kilometers: 7 * CUBIT_IN_KILOMETERS, // 1 reed = 6 long cubits = 7 cubits
+      meters: 7 * CUBIT_IN_METERS, 
+      centimeters: 7 * CUBIT_IN_CM,
+      inches: 7 * CUBIT_IN_INCHES,
+      yards: 7 * CUBIT_IN_YARDS
     }
-  }
+  },
+  shabbath_day: {
+    category: "length",
+    conversions: {
+      kilometers: 200 * 7 * CUBIT_IN_KILOMETERS / 6, // 1 shabbath's day = 200 nt cubits = 200 Old Testament long cubits
+      meters: 200 * 7 * CUBIT_IN_METERS / 6, 
+      centimeters: 200 * 7 * CUBIT_IN_CM / 6,
+      inches: 200 * 7 * CUBIT_IN_INCHES / 6,
+      yards: 200 * 7 * CUBIT_IN_YARDS / 6
+    }
+  },
+  fathom: {
+    category: "length",
+    conversions: {
+      kilometers: 1.8/1000,
+      meters: 1.8, 
+      centimeters: 180,
+      inches: 180/2.54,
+      yards: 180/(2.54*36)
+    }
+  },
+  nt_cubit: {
+    category: "length",
+    conversions: {
+      kilometers: 7 * CUBIT_IN_KILOMETERS / 6, // An NT cubit is equivalent to an OT long cubit
+      meters: 7 * CUBIT_IN_METERS / 6,
+      centimeters: 7 * CUBIT_IN_CM / 6,
+      inches: 7 * CUBIT_IN_INCHES / 6,
+      yards: 7 * CUBIT_IN_YARDS / 6
+    }
+  },
+  stade: {
+    category: "length",
+    conversions: {
+      kilometers: 185/1000,
+      meters: 185, 
+      centimeters: 185*100,
+      inches: 185*100/2.54,
+      yards: 185*100/(2.54*36)
+    }
+  },
+  roman_mile: {
+    category: "length",
+    conversions: {
+      kilometers: 1497/1000,
+      meters: 1497, 
+      centimeters: 1497*100,
+      inches: 1497*100/2.54,
+      yards: 1497*100/(2.54*36)
+    }
+  },
+  talent: {
+    category: "weight",
+    conversions: {
+      kilograms: 34.3, // For simplicity, this could also be derived from a base weight unit if applicable
+      grams: 34.3 * 1000,
+      pounds: 34.3 * 2.20462
+    }
+  },
   
 };
 
